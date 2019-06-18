90599132c6e12290f2df097ce99142425390bf84
Corporate Happienss - IT <it@corporate-happiness.de>
2019-06-18 15:17:33 +0200

    Initial Commit

---
+++ CohaQuoteSlider.php
@@ -0,0 +1,89 @@
+<?php 
+
+namespace CohaBeautifulEmotion;
+
+use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\Common\Collections\ArrayCollection;
+use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Components\Plugin\Context\UninstallContext;
+use Shopware\Components\Theme\LessDefinition;
+use Shopware\Components\Plugin\Context\ActivateContext;
+
+class CohaBeautifulEmotion extends Plugin
+{
+
+    public function install(InstallContext $context)
+    {
+        $service = $this->container->get('shopware_attribute.crud_service');
+
+        $service->update('s_categories_attributes', 'coha_navigation_space', 'boolean', [
+            'label' => 'Space to the Right',
+            'supportText' => 'Select if This Menu should create Space on the Right side',
+            'helpText' => 'If this is Checked, a few pixels (about 200-300px) will be spaced to the right',
+
+            //user has the opportunity to translate the attribute field for each shop
+            'translatable' => true,
+
+            //attribute will be displayed in the backend module
+            'displayInBackend' => true,
+
+            //numeric position for the backend view, sorted ascending
+            'position' => 250,
+
+            //user can modify the attribute in the free text field module
+            'custom' => true,
+        ]);
+    }
+
+    // On Activation
+    public function activate(ActivateContext $context)
+    {
+        $context->scheduleClearCache(ActivateContext::CACHE_LIST_ALL);
+    }
+
+    public function uninstall(UninstallContext $context)
+    {
+        $service = $this->container->get('shopware_attribute.crud_service');
+        $service->delete('s_emotion_attributes', 'coha_font_color');
+    }
+
+    public function addLessFiles(){
+        return new LessDefinition(
+            [],
+            [
+                // __DIR__ . '/Resources/views/frontend/_public/src/css/ [...] .css ',
+            ]
+        );
+    }
+
+    public function onCollectJavascriptFiles()
+    {
+        $jsFiles = [
+            // $this->getPath() . '/Resources/views/frontend/_public/ [...] .js',
+        ];
+        return new ArrayCollection($jsFiles);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Enlight_Controller_Action_PreDispatch_Frontend' => ['onFrontend',-100],
+            'Enlight_Controller_Action_PreDispatch_Widgets' => ['onFrontend',-100],
+            'Theme_Compiler_Collect_Plugin_Less' => 'addLessFiles',
+            'Theme_Compiler_Collect_Plugin_Javascript' => 'onCollectJavascriptFiles',
+        ];
+    }
+
+    /**
+     * @param \Enlight_Event_EventArgs $args
+     * @throws \Exception
+     */
+    public function onFrontend(\Enlight_Event_EventArgs $args)
+    {
+        $this->container->get('Template')->addTemplateDir(
+            $this->getPath() . '/Resources/views/'
+        );
+    }
+
+}
